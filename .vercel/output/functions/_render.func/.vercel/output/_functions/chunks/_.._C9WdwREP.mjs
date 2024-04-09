5d57fb9ffb678725e9374202c0b7b4bd052b0f4e
Yash <78804783+yashd-dev@users.noreply.github.com>
2024-04-09 23:25:22 +0530

    Testing DYnamic Blogs

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/_.._C9WdwREP.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/__otQSqNGY.mjs').then(n => n._);
-
-export { page };
--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_CpzrH3S-.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_DCg_eSPW.mjs').then(n => n.g);
-
-export { page };
